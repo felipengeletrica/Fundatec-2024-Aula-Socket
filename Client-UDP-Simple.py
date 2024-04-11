f22db219f97edf9a15069a8f39759bcbfda7fd55
Gustavo-Waschburger <76873670+Gustavo-Waschburger@users.noreply.github.com>
2024-04-11 20:27:40 -0300

    Update Client-UDP-Simple.py
    Alteração das mensagens

--- Client-UDP-Simple.py
+++ Client-UDP-Simple.py
@@ -6,11 +6,11 @@ HOST, PORT = "localhost", 65431
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-    data = input("Message:")
+    data = input("Mensagem:")
     # As you can see, there is no connect() call; UDP has no connections.
     # Instead, data is directly sent to the recipient via sendto().
     sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
     received = str(sock.recv(1024), "utf-8")
 
-    print("Sent:     {}".format(data))
-    print("Received: {}".format(received))
+    print("Enviar:     {}".format(data))
+    print("Recebido: {}".format(received))
