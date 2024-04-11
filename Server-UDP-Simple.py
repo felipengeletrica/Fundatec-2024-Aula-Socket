d196c2b5a7d4ef59e74f64d52bb130a9f18db827
Gustavo-Waschburger <76873670+Gustavo-Waschburger@users.noreply.github.com>
2024-04-11 20:28:51 -0300

    Update Server-UDP-Simple.py
    alteração de mensagem

--- Server-UDP-Simple.py
+++ Server-UDP-Simple.py
@@ -11,7 +11,7 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
-        print("{} wrote:".format(self.client_address[0]))
+        print("{} escreveu:".format(self.client_address[0]))
         print(data)
         socket.sendto(data.upper(), self.client_address)
 
