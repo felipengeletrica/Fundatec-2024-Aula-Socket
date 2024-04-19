2b469d3afd6cbbf801899e89b9e99425eb48a8e5
schmitt <130003783+ximitt@users.noreply.github.com>
2024-04-18 21:08:17 -0300

    Update Server-TCP-simple.py

--- Server-TCP-simple.py
+++ Server-TCP-simple.py
@@ -1,19 +1,28 @@
-# echo-server.py
-import socket
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+import socket  # Importa o módulo socket para comunicação de rede.
+
+HOST = "10.0.1.132"  # Endereço IP padrão da interface de loopback (localhost)
+PORT = 65432  # Porta para escutar (portas não privilegiadas são > 1023)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+    # Cria um objeto socket usando IPv4 (AF_INET) e TCP (SOCK_STREAM)
+    
+    s.bind((HOST, PORT))  # Associa o socket ao endereço e porta especificados
     print("Bind")
-    s.listen()
+    
+    s.listen()  # Coloca o socket em modo de escuta para aceitar conexões
     print("Listen")
-    conn, addr = s.accept()
+    
+    conn, addr = s.accept()  # Aceita uma conexão e retorna um novo socket e o endereço do cliente
     print("Connected")
-    with conn:
-        print(f"Connected by {addr}")
+    
+    with conn:  # Garante que o socket de conexão seja fechado corretamente quando sair do bloco 'with'
+        print(f"Connected by {addr}")  # Imprime o endereço do cliente conectado
+        
         while True:
-            data = conn.recv(1024)
-            print(str(data))
-            if not data:
+            data = conn.recv(1024)  # Recebe dados do cliente
+            print(str(data))  # Imprime os dados recebidos do cliente
+            
+            if not data:  # Se não houver mais dados a serem recebidos, encerra o loop
                 break
-            conn.sendall(data)
+            
+            conn.sendall(data)  # Envia de volta os dados recebidos para o cliente
