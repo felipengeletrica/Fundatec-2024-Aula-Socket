24a4c98167f190291177e7ad1c2b745fe218491f
DiogoBrum19 <130003743+DiogoBrum19@users.noreply.github.com>
2024-04-18 21:08:05 -0300

    Update Server-TCP-Multicliente.py

--- Server-TCP-Multicliente.py
+++ Server-TCP-Multicliente.py
@@ -1,31 +1,28 @@
 import socketserver
 
-
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
-    The RequestHandler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
+    A classe RequestHandler para nosso servidor.
+    É instanciada uma vez por conexão com o servidor e deve
+    substituir o método handle() para implementar a comunicação
+    com o cliente.
     """
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
+        # self.request é o socket TCP conectado ao cliente
+        self.data = self.request.recv(1024).strip()  # Recebe dados do cliente
+        print("{} wrote:".format(self.client_address[0]))  # Exibe o endereço do cliente
+        print(self.data)  # Exibe os dados recebidos
+        # Envia de volta os mesmos dados em maiúsculas para o cliente
         self.request.sendall(self.data.upper())
-
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 65432
 
-    # Create the server, binding to localhost on port 9999
+    # Cria o servidor, vinculando ao localhost na porta 65432
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.timeout = 10
-    server.serve_forever()
+    # Ativa o servidor; ele continuará em execução até que você
+    # interrompa o programa com Ctrl-C
+    server.timeout = 10  # Define um tempo limite de inatividade de 10 segundos
+    server.serve_forever()  # Mantém o servidor em execução indefinidamente
