57218899f4fc6b9ae4e2521b9bf9100db4a70468
schmitt <130003783+ximitt@users.noreply.github.com>
2024-04-18 21:07:59 -0300

    Update Server-TCP-Multicliente.py

--- Server-TCP-Multicliente.py
+++ Server-TCP-Multicliente.py
@@ -1,31 +1,30 @@
-import socketserver
-
+import socketserver  # Importa o módulo socketserver, que fornece uma infraestrutura para servidores de rede.
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     """
-    The RequestHandler class for our server.
+    A classe RequestHandler para o nosso servidor.
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
+    É instanciado uma vez por conexão com o servidor e deve
+    substituir o método handle() para implementar a comunicação com o
+    cliente.
     """
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        # self.request é o socket TCP conectado ao cliente
+        self.data = self.request.recv(1024).strip()  # Recebe dados do cliente através do socket e remove espaços em branco.
+        print("{} wrote:".format(self.client_address[0]))  # Imprime o endereço IP do cliente.
+        print(self.data)  # Imprime os dados recebidos do cliente.
+        # apenas envia de volta os mesmos dados, mas em maiúsculas
+        self.request.sendall(self.data.upper())  # Envia os dados recebidos de volta para o cliente, convertidos para maiúsculas.
 
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 65432
 
-    # Create the server, binding to localhost on port 9999
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    # Cria o servidor, vinculando ao localhost na porta 65432
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)  # Cria um servidor TCP na máquina local (localhost) e na porta 65432, usando a classe MyTCPHandler para lidar com as solicitações.
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.timeout = 10
-    server.serve_forever()
+    # Ativa o servidor; isso continuará em execução até que você
+    # interrompa o programa com Ctrl-C
+    server.timeout = 10  # Define um timeout de 10 segundos para o servidor.
+    server.serve_forever()  # Inicia o servidor e continua executando para sempre, lidando com conexões de clientes.
