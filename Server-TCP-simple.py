acae76b583f24c088e06ed2e48bd2a1f19261773
DiogoBrum19 <130003743+DiogoBrum19@users.noreply.github.com>
2024-04-18 21:08:17 -0300

    Update Server-TCP-simple.py

--- Server-TCP-simple.py
+++ Server-TCP-simple.py
@@ -1,19 +1,29 @@
-# echo-server.py
 import socket
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+# Definindo o endereço e porta do servidor
+HOST = "10.0.1.143"  # Endereço de loopback (localhost)
+PORT = 65432  # Porta para escutar
+
+# Criando um socket TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Vinculando o socket ao endereço e porta especificados
     s.bind((HOST, PORT))
-    print("Bind")
+    print("Bind")  # Mensagem de confirmação de vínculo
+    # Configurando o socket para escutar conexões entrantes
     s.listen()
-    print("Listen")
+    print("Listen")  # Mensagem de confirmação de escuta
+    # Aceitando a conexão entrante
     conn, addr = s.accept()
-    print("Connected")
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            print(str(data))
-            if not data:
-                break
-            conn.sendall(data)
+    print("Connected")  # Mensagem de confirmação de conexão
+    # Exibindo o endereço do cliente conectado
+    print(f"Connected by {addr}")
+
+    # Loop para receber e enviar dados
+    while True:
+        # Recebendo dados do cliente
+        data = conn.recv(1024)
+        print(str(data))  # Exibindo os dados recebidos
+        if not data:
+            break  # Se não houver mais dados, encerra o loop
+        # Enviando de volta os dados recebidos para o cliente
+        conn.sendall(data)
