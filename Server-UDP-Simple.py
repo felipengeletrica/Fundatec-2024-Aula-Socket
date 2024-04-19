2d6ec11aa2a44a66211cda1efa00d88495ef0d13
DiogoBrum19 <130003743+DiogoBrum19@users.noreply.github.com>
2024-04-18 21:09:15 -0300

    Update Server-UDP-Simple.py

--- Server-UDP-Simple.py
+++ Server-UDP-Simple.py
@@ -2,20 +2,27 @@ import socketserver
 
 class MyUDPHandler(socketserver.BaseRequestHandler):
     """
-    This class works similar to the TCP handler class, except that
-    self.request consists of a pair of data and client socket, and since
-    there is no connection the client address must be given explicitly
-    when sending data back via sendto().
+    Esta classe funciona de forma semelhante à classe de manipulador TCP, exceto que
+    self.request consiste em um par de dados e um socket do cliente, e como
+    não há conexão, o endereço do cliente deve ser fornecido explicitamente
+    ao enviar dados de volta via sendto().
     """
 
     def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
+        # Extrai os dados recebidos e o socket do cliente
+        data = self.request[0].strip()  # Extrai os dados
+        socket = self.request[1]  # Extrai o socket do cliente
+        # Exibe o endereço do cliente e os dados recebidos
         print("{} wrote:".format(self.client_address[0]))
         print(data)
+        # Envia de volta os dados em maiúsculas para o cliente
         socket.sendto(data.upper(), self.client_address)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 65431
+
+    # Cria o servidor UDP, vinculando ao localhost na porta 65431
     with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
+        # Ativa o servidor; ele continuará em execução até que você
+        # interrompa o programa com Ctrl-C
         server.serve_forever()
